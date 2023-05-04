d034ab9e134e3bc9462a5107cd7b34f99219c4d4
EliasMCR <121818883+EliasMCR@users.noreply.github.com>
2023-05-04 19:49:09 -0300

    feitos atividades

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var km = 80
+console.log(`A velocidade do seu carro é de ${km}Km/h`)
+if (km > 80) {
+    console.log(`Sua velocidade é de ${km}Km/h, MULTADO`)
+}
+console.log('Dirija sempre com cinto de segurança')
