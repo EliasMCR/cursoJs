268f443f7526be7db8d5835f3ab5e1f3790a40a5
EliasMCR <121818883+EliasMCR@users.noreply.github.com>
2023-07-05 16:29:01 -0300

    k

---
+++ Aula15/ambiente.js
@@ -0,0 +1,13 @@
+/*var vetor = [1,2,3,4,5]
+
+for(let i = 0; i < vetor.length; i++)
+{
+    console.log(vetor[i])
+}*/
+
+let vetor = [0,1,2,3,4,5]
+
+for(let i in vetor)
+{
+    console.log(vetor[i])
+}
---
+++ Aula16/funca01.js
@@ -0,0 +1,13 @@
+function parImpar(n)
+{
+    if(n%2 == 0)
+    {
+        console.log("par")
+    }
+    else
+    {
+        console.log("impar")
+    }
+}
+
+let c = parImpar(1)
